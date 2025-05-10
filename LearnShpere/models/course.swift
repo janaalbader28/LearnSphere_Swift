b7c63cb1e4dcc757fc5ae4f4646ce4726b6fee18
Jana Albader <159451008+janaalbader28@users.noreply.github.com>
2025-05-10 23:49:49 +0300

    Add files via upload

---
+++ LearnShpere/models/course.swift
@@ -0,0 +1,18 @@
+//
+//  Course.swift
+//  LearnSphere
+//
+//  Created by Jana on 09/05/2025.
+//
+
+
+struct Course {
+    let title: String
+    let info: String
+    let date: String
+    let location: String
+    let imagePath: String
+    let url: String
+    let type: String
+    var id: Int64
+}
