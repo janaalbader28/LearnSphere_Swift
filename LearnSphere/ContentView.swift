4271124855558b2d976e4dfa1443fe51f209e69d
Jana Albader <159451008+janaalbader28@users.noreply.github.com>
2025-05-11 00:22:07 +0300

    Add files via upload

---
+++ LearnSphere/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  LearnSphere
+//
+//  Created by Shamma Sami on 11/11/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       Home()
+    }
+}
+
+#Preview {
+    ContentView()
+}
